49ee6c104549171a824546906fad84fb4e9d35a0
Anujith vk <anolianu123@gmail.com>
2025-05-23 12:24:45 +0530

    service-changes

--- components/pages/services/service-single/services-single.jsx
+++ components/pages/services/service-single/services-single.jsx
@@ -1,13 +1,14 @@
-'use client'
+'use client';
 import ServicesDetail from '../../../../public/assets/img/service/service-details.png';
 import image1 from '../../../../public/assets/img/icon/service-details-icon-2.png';
 import image2 from '../../../../public/assets/img/icon/service-details-icon.png';
 import { useParams } from 'next/navigation';
 import servicesData from '@/components/data/services-data';
 
-const ServicesSingleMain = ({ firstAndSecondWord}) => {
- const {id} = useParams();
- const service=servicesData.find((service)=>service.id === id)
+const ServicesSingleMain = ({ firstAndSecondWord }) => {
+  const { id } = useParams();
+  const service = servicesData.find((service) => service.id === id);
+
   return (
     <div className="service__details section-padding">
       <div className="container">
@@ -49,6 +50,25 @@ const ServicesSingleMain = ({ firstAndSecondWord}) => {
               </div>
 
               <p>{service?.p5}</p>
+            <div className='container'>
+              {service?.Workflow && (
+                <div className="">
+                  <h3 className="sub-heading mb-4">{service?.p6}</h3>
+                  <div className='service__details-content-box-single'>
+                        {service.Workflow.map((section, idx) => (
+                    <div key={idx} className="mb-4">
+                      <h5 className="font-bold">{section.title}</h5>
+                      <ul className="">
+                        {section.items.map((item, subIdx) => (
+                          <li key={subIdx}>{item}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  ))}
+                  </div>
+                </div>
+              )}
+              </div>
             </div>
           </div>
         </div>

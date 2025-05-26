c687c941bcc41ab5c62f8e703a364bb690df6aa8
Anujith vk <anolianu123@gmail.com>
2025-05-26 16:54:18 +0530

    contact-image-added

--- components/pages/contacts/MainContact.jsx
+++ components/pages/contacts/MainContact.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormArea from './form';
+import Image from 'next/image';
 
 const MainContact = () => {
   return (
@@ -22,46 +23,17 @@ const MainContact = () => {
             </div>
             <div className="col-xl-6">
               <div className="contact__two-contact-info">
+
+
                 <div className="contact__two-single-info">
-                  <div className="contact__two-single-info-icon">
-                    <img src="/assets/img/icon/service-1.png" alt="Email" />
+                  <div >
+                     <Image src='/assets/img/vijeesh_sir.jpg' width={150} height={150} className='rounded-full'/>
                   </div>
                   <div className="contact__two-single-info-content">
-                    <h4>Email</h4>
-                    <span>contact@coderzon.com</span>
+                    <h4>Vijeesh Tp</h4>
+                    <span>Head of Sales & Account Executive</span>
                   </div>
-                </div>
-                <div className="contact__two-single-info">
-                  <div className="contact__two-single-info-icon">
-                    <img src="/assets/img/icon/service-2.png" alt="Phone" />
-                  </div>
-                  <div className="contact__two-single-info-content">
-                    <h4>Contact</h4>
-                    <span>(+91) 8301890158</span>
-                  </div>
-                </div>
-                <div className="contact__two-single-info">
-                  <div className="contact__two-single-info-icon">
-                    <img src="/assets/img/icon/service-3.png" alt="Availability" />
-                  </div>
-                  <div className="contact__two-single-info-content">
-                    <h4>Available</h4>
-                    <span>Monday - Friday</span>
-                    <span>9.Am To 6.Pm</span>
-                  </div>
-                </div>
-                <div className="contact__two-single-info">
-                  <div className="contact__two-single-info-icon">
-                    <img src="/assets/img/icon/service-4.png" alt="Location" />
-                  </div>
-                  <div className="contact__two-single-info-content">
-                    <h4>Location</h4>
-                    <span>
-                      CODERZON Technologies Pvt Ltd, AKL Heights, Seaport Road,
-                      Near Bharathmatha College, Kochi, Kerala - 682021
-                    </span>
-                  </div>
-                </div>
+                </div>                   
               </div>
             </div>
           </div>

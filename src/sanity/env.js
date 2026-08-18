86d4e36e9459068abde3ffbd7d1d2bfbee22b1ed
Anujith vk <anolianu123@gmail.com>
2026-08-18 11:39:23 +0530

    dev push

--- src/sanity/env.js
+++ src/sanity/env.js
@@ -1,5 +1,3 @@
-/** Sanity connection details, read from environment variables. */
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-05-14";
+export const apiVersion =process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-05-14";
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;

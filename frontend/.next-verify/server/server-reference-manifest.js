8fe65c0f3cdfe8d07953f99503cad8f919906864
Anujith vk <anujith.vk@coderzon.com>
2026-08-24 10:41:10 +0530

    env variables corrected

--- frontend/.next-verify/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"3mKLEGgL6BX6oZ7OKRRkx86rJkfjqtrYxrWurcUOUhQ=\"}"
--- frontend/.next-verify/static/chunks/app/not-found-fc9ad55b5888940b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{1418:function(e,n,s){Promise.resolve().then(s.t.bind(s,8173,23)),Promise.resolve().then(s.t.bind(s,231,23)),Promise.resolve().then(s.bind(s,2158)),Promise.resolve().then(s.bind(s,261))}},function(e){e.O(0,[231,762,863,173,592,199,29,783,971,23,744],function(){return e(e.s=1418)}),_N_E=e.O()}]);
